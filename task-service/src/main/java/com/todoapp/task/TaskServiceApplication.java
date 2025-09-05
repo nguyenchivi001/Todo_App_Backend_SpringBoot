deb74b33ee2a1bc8a0eec947da48e020cc3e4690
nguyenchivi001 <nguyenchivi292004@gmail.com>
2025-09-05 07:30:26 +0700

    Create application.properties and Dockerfile for task service

--- task-service/src/main/java/com/todoapp/task/TaskServiceApplication.java
+++ task-service/src/main/java/com/todoapp/task/TaskServiceApplication.java
@@ -2,8 +2,12 @@ package com.todoapp.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableTransactionManagement
 public class TaskServiceApplication {
 
 	public static void main(String[] args) {
