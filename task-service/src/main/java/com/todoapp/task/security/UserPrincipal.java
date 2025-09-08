c6ed711e42bdae677675734c549cbf230bc5b30c
nguyenchivi001 <nguyenchivi292004@gmail.com>
2025-09-08 17:38:23 +0700

    Config security and user principal

---
+++ task-service/src/main/java/com/todoapp/task/security/UserPrincipal.java
@@ -0,0 +1,67 @@
+package com.todoapp.task.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+    private String username;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null; // Not used in JWT authentication
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserPrincipal)) return false;
+        UserPrincipal that = (UserPrincipal) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
